113d14b9ed771e87c80ad5683b7d1d53d6496578
vikram <vikram2code@gmail.com>
2024-01-16 10:54:00 +0530

    Added boostrap and its css ,Script

--- header.php
+++ header.php
@@ -27,7 +27,12 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <!-- Latest compiled JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
+        <!-- Include Bootstrap JS and Popper.js -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <!-- Include Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
         <style>
           header.fixed-top {
           background-image: url('<?php echo get_template_directory_uri()?>/assets/img/background/background.jpg'); /* Replace 'path/to/your/image.jpg' with the actual path to your image */
