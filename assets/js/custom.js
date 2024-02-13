53d04a993a40cb5c5bf874d4472b9a68d80fc6d1
vikram <vikram2code@gmail.com>
2024-02-13 16:30:06 +0530

    Added the Model light and shadow

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -171,7 +171,7 @@ $(document).ready(function () {
         $('.modal-title').text(decodedFileName);
 
         // Add the model-viewer to the modal
-        modalModelContainer.append('<model-viewer src="' + modelSrc + '" camera-controls auto-rotate id="modal_model" style="width: 100%;height:530px" exposure="1.0" shadow-intensity="1.5" background-color="#f0f0f0" shadow-softness="0.5" loading="lazy" poster="<?php echo esc_url(get_template_directory_uri()) ?>/forerunner_car_poster.jpg"></model-viewer>');
+        modalModelContainer.append('<model-viewer src="' + modelSrc + '" camera-controls auto-rotate id="modal_model" style="width: 100%;height:530px" shadow-intensity="10.5" background-color="#232323" shadow-softness="0.5" loading="lazy" poster="<?php echo esc_url(get_template_directory_uri()) ?>/forerunner_car_poster.jpg"></model-viewer>');
     });
 
     // Function to handle model click event on the right side
